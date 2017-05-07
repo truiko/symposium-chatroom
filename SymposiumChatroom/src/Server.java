d41d15c8a00fe7afb10767e6354474b78646811d
hotfi <hotfi@DESKTOP-H975P1D.fios-router.home>
2017-05-07 18:12:13 -0400

    starting creating server class

--- SymposiumChatroom/src/Server.java
+++ SymposiumChatroom/src/Server.java
@@ -1,13 +1,52 @@
-import java.io.*;
-import java.net.*;
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class Server extends JFrame {
+	
+	private JTextField userText;
+	private JTextArea chatWindow;
+	private ObjectOutputStream output;
+	private ObjectInputStream input;
+	private ServerSocket server;
+	private Socket connection;
 
 	public Server() {
-
+		super("Buckys Instant Messenger");
+		userText = new JTextField();
+		userText.setEditable(false);
+		userText.addActionListener(
+				new ActionListener(){
+					public void actionPerformed(ActionEvent event){
+						sendMessage(event.getActionCommand());
+						userText.setText("");
+					}
+				}
+		);
+		add(userText , BorderLayout.NORTH);
+		chatWindow = new JTextArea();
+		add(new JScrollPane(chatWindow));
+		setSize(300,150);
+		setVisible(true);
 	}
 
+	//set up and run the server
+	public void startRunning(){
+		try{
+			server = new ServerSocket(6789, 100);
+		}catch(IOException ioException){
+			ioException.printStackTrace();
+		}
+	}
 }

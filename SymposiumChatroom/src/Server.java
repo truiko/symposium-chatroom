65c1dd540a8b9baef21dea5f368322e2dca4f06b
Student 8 <Student 8@10.8.33.94>
2017-05-09 13:53:27 -0400

    updated server

--- SymposiumChatroom/src/Server.java
+++ SymposiumChatroom/src/Server.java
@@ -61,9 +61,60 @@ public class Server extends JFrame {
 			ioException.printStackTrace();
 		}
 	}
-	
-	//wait for connection, then display connextion information
+
+	//wait for connection, then display connection information
 	private void waitForConnection() throws IOException{
-		showMessage("Waiting")
+		showMessage("Waiting for someone to connect... \n");
+		connection = server.accept();
+		showMessage("Now connected to "+ connection.getInetAddress().getHostAddress());
+	}
+	
+	//get stream to send and receive data
+	private void setupStreams() throws IOException {
+		output = new ObjectOutputStream(connection.getOutputStream());
+		output.flush();
+		input = new ObjectInputStream(connection.getInputStream());
+		showMessage("\n Streans are now setup! \n");
+		
+	}
+	
+	//during the chat conversation
+	private void whileChatting() throws IOException{
+		String message = " You are now connected! ";
+		sendMessage(message);
+		ableToType(true);
+		do{
+			try{
+				message = (String) input.readObject();
+				showMessage("\n" + message);
+			}catch(ClassNotFoundException classNotFoundException){
+				showMessage("\n Can't understand what that user sent!");
+			}
+		}while(!message.equals("CLIENT - END"));
+	}
+	
+	//close streams and sockets after you are done chatting
+	private void closeAll(){
+		showMessage("\n Closing connections... \n");
+		ableToType(false);
+		try{
+			output.close();
+			input.close();
+			connection.close();
+		}catch(IOException ioException){
+			ioException.printStackTrace();
+		}
+	}
+	
+	//send a message to client
+	private void sendMessage(String message){
+		try{
+			output.writeObject("SERVER - "+ message);
+			output.flush();
+			showMessage("\nSERVER - " + message);
+		}catch(IOException ioException){
+			chatWindow.append("\n ERROR: Message can't be sent");
+		}
 	}
 }
+	

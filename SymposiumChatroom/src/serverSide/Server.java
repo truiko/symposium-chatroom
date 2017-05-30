c332edf2d1a265858641d5c22d2318f02a1bbbe7
Student 8 <Student 8@10.8.33.156>
2017-05-30 13:56:15 -0400

    trying to make server show image sent from client

--- SymposiumChatroom/src/serverSide/Server.java
+++ SymposiumChatroom/src/serverSide/Server.java
@@ -3,14 +3,21 @@ package serverSide;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.EOFException;
+import java.io.File;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
@@ -24,6 +31,8 @@ public class Server extends JFrame {
 	private ObjectInputStream input;
 	private ServerSocket server;
 	private Socket connection;
+	
+	private JButton attachment;
 
 	public Server() {
 		super("Symposium Server");
@@ -43,6 +52,22 @@ public class Server extends JFrame {
 		add(new JScrollPane(chatWindow));
 		setSize(300,150);
 		setVisible(true);
+		
+		attachment = new JButton("Attachment");
+		attachment.setSize(100,100);
+		add(attachment, BorderLayout.EAST);
+		attachment.setVisible(true);
+		
+		attachment.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				try {
+					sendImage();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
 	}
 //
 	//set up and run the server
@@ -90,6 +115,7 @@ public class Server extends JFrame {
 			try{
 				message = (String) input.readObject();
 				showMessage("\n" + message);
+				receiveImage();
 				showMessage("hi");
 			}catch(ClassNotFoundException classNotFoundException){
 				showMessage("\n Can't understand what that user sent!");
@@ -141,6 +167,36 @@ public class Server extends JFrame {
 				}
 			}
 		);
+	}
+	private void sendImage() throws IOException{
+		BufferedImage img = null;
+		JFileChooser fc = new JFileChooser();
+		int returnVal = fc.showOpenDialog(fc);
+		String filePath = null;
+		if(returnVal == JFileChooser.APPROVE_OPTION){
+			filePath = fc.getSelectedFile().getAbsolutePath();
+		}else{
+			System.out.println("User clicked CANCEL");
+			System.exit(1);
+		}
+		try{
+			img = ImageIO.read(new File(filePath));
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+        ImageIO.write(img, "jpg", connection.getOutputStream());
+        System.out.println("sent");
+	}
+	
+	private void receiveImage() throws IOException{
+		BufferedImage image = ImageIO.read(connection.getInputStream());
+	      System.out.println("got image");
+	      JLabel label = new JLabel(new ImageIcon(image));
+	      JFrame f = new JFrame("Image sent from server");
+	      f.getContentPane().add(label);
+	      f.pack();
+	      f.setVisible(true);
+	      System.out.println("image is displayed");
 	}
 }
 	

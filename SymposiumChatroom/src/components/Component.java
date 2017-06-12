4816aa7b99c36ee07305187950146dae934638fc
Veeraj <Period82.0@10.8.33.160>
2017-06-12 13:56:48 -0400

    eys

---
+++ SymposiumChatroom/src/components/Component.java
@@ -0,0 +1,9 @@
+package components;
+
+public class Component {
+
+	public Component() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ SymposiumChatroom/src/components/TextLabel.java
@@ -0,0 +1,9 @@
+package components;
+
+public class TextLabel {
+
+	public TextLabel() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
